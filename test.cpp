5b57939b0a76d8be73b552f6fe5489175e0ebb86
WeitingWei <35938073+WeitingWei@users.noreply.github.com>
2018-05-03 23:55:43 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -23,3 +23,25 @@ if (testList -> isEmpty() == true)
 else{
     cout << "not success"<<endl;
 }
+void Test::addBackTest()
+{
+    cout << "Begin addBack function test"<<endl;
+
+    LinkedListOfInts* testList = new LinkedListOfInts();
+
+    try
+    {
+        cout << "Adding";
+        for(auto it = intList.begin(); it != intList.end(); it++)
+        {
+            testList->addBack(*it);
+        }
+    
+        if(testList->size() == 100)
+        {
+            cout << "pass." <<endl;
+        }
+        else
+        {
+            cout << "fail, " << testList->size() << "elememt(s) in the list." <<endl;
+        }
